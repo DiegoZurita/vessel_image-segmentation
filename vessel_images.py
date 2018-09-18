8a71e8742ddbf94da036c52c9884cea6219be056
Diego Zurita <diego.zurita@usp.br>
2018-09-17 22:28:24 -0300

    Start the cnn approach

---
+++ vessel_images.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+class VesselImages:
+
+    def __init__(self, root_images_folder):
+        self.root_images_folder = root_images_folder
+        self.TEST_FOLDER = "test"
+        self.TRAIN_FOLDER = "training"
+
+    def get_train_test_images(self):
+        pass
+    
+    def get_images_uris(self):
+        pass
