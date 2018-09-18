c64f947da56f3fcce7068a47349eae30db355055
Diego Zurita <diego.zurita@usp.br>
2018-09-18 12:31:57 -0300

    Corrrecao de codigo e implementada funcionalidade de preparar dados para o modelo.

---
+++ train.py
@@ -0,0 +1,12 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from vessel_images import VesselImages
+
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
--- vessel_images.py
+++ vessel_images.py
@@ -79,25 +79,63 @@ class VesselImages:
         image_cropped = image[pixel[0] + cima - middle_square : pixel[0] - baixo + middle_square , \
                                pixel[1] + esquerda - middle_square : pixel[1]  - direita + middle_square]
     
-        self.pad_image(
-            image_cropped, 
-            {
-                "cima": cima,
-                "baixo": baixo,
-                "esquerda": esquerda,
-                "direita": direita
-            },
-            (self.square_size, self.square_size, 3),
-            0
-        )
-
-        return image_cropped
+        
+        if image_cropped.shape[0] == self.square_size and image_cropped.shape[1] == self.square_size:
+            return image_cropped
+        else:
+            return self.pad_image(
+                image_cropped, 
+                {
+                    "cima": cima,
+                    "baixo": baixo,
+                    "esquerda": esquerda,
+                    "direita": direita
+                },
+                (self.square_size, self.square_size, 3),
+                0
+            )
 
     def pad_image(self, image, coord, final_shape, value):
         new_image = value * np.ones(final_shape, dtype=int)
         
-        new_image[coord["cima"]: final_shape[0] - coord["baixo"], coord["esquerda"]: final_shape[1] - coord["direita"]] = image
-
-        plt.imshow(new_image)
-        plt.show()        
+        new_image[coord["cima"]: final_shape[0] - coord["baixo"], coord["esquerda"]: final_shape[1] - coord["direita"]] = image  
         return new_image
+
+    def prepare_data_for_the_model(self, images, masks):
+        pixels_data_from_mask = self.discriminate_masks(masks)
+
+        y = None
+        X = []
+        
+        for i in range(images.shape[0]):
+            image_data = pixels_data_from_mask[i]
+            vessel_pixels = image_data[0]
+            not_vessel_pixels = self.get_random_not_vessel_pixels(image_data[1], 2 * len(vessel_pixels))
+
+            
+            vessels_output = np.ones((vessel_pixels.shape[0], 2))
+            vessels_output[:, 1:] = vessels_output[:, 1:] * 0
+
+            not_vessels_output = np.ones((not_vessel_pixels.shape[0], 2))
+            not_vessels_output[:, :1] = not_vessels_output[:, :1] * 0
+
+            pixels_concat = np.concatenate((vessel_pixels, not_vessel_pixels), axis=0)
+            y_concat = np.concatenate((vessels_output, not_vessels_output), axis=0)
+
+            for pixel in pixels_concat:
+                X.append(self.get_square_of_pixel(images[i], pixel))
+
+            if y is None:
+                y = y_concat
+            else:
+                y = np.concatenate((y, y_concat), axis=0)
+
+        X = np.array(X)
+
+        #shufle
+        indexes = np.random.permutation(y.shape[0])
+
+        X = X[indexes]
+        y = y[indexes]
+
+        return X, y
